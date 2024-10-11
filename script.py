3b18cb69e4d739f614f900c990b9d0f02bd1b3e2
dutmar <darjan.utmar@gmail.com>
2024-10-11 21:53:00 +0200

    Added recognization of which hand is shown and tweaked text shown on display to refresh when hand is not shown. Added class for important hand data

--- script.py
+++ script.py
@@ -2,6 +2,15 @@ import cv2
 import mediapipe as mp
 
 CALIBRATION_FRAMES = 30
+FRAMES_ELAPSED = 0
+
+class HandData:
+    isWaving = False
+    fingers = None
+    left_hand = False
+    right_hand = False
+    prev_hand_x = None
+    movement = 0
 
 # Initialize MediaPipe Hands module
 mp_hands = mp.solutions.hands
@@ -13,20 +22,17 @@ mp_drawing = mp.solutions.drawing_utils
 # Open a video capture object (0 for the default camera)
 cap = cv2.VideoCapture(0)
 
-frames_elapsed = 0
-isWaving = False
-fingers = None
-prev_hand_x = None
-movement = 0
-
 def writeOnImage(frame):
     global isWaving, fingers
-    if frames_elapsed < CALIBRATION_FRAMES:
+    if FRAMES_ELAPSED < CALIBRATION_FRAMES:
         text = "Calibrating..."
-    elif isWaving:
+    elif HandData.isWaving:
         text = "Waving"
-    elif fingers and not isWaving:
-        text = str(fingers)
+    elif HandData.fingers is not None:
+        if HandData.fingers > 0:
+            text = str(HandData.fingers)
+        else:
+            text = "None"
     else:
         text = "None"
 
@@ -34,19 +40,19 @@ def writeOnImage(frame):
     cv2.putText(frame, text, (10,20), cv2.FONT_HERSHEY_COMPLEX, 0.8,(255,255,255),1,cv2.LINE_AA)
 
 def checkWaving(movement):
-    global isWaving
+    # global HandData.isWaving
 
     if abs(movement) > 10:
-        isWaving = True
+        HandData.isWaving = True
     else:
-        isWaving = False
+        HandData.isWaving = False
 
 def isFingerExtended(hand_landmarks, finger_tip_id, finger_dip_id):
     # If top part of finger is higher then "middle" part
     return hand_landmarks.landmark[finger_tip_id].y < hand_landmarks.landmark[finger_dip_id].y
 
 def countFingers(hand_landmarks):
-    global fingers
+    # global fingers
 
     thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x > hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].x
     index = isFingerExtended(hand_landmarks, mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.INDEX_FINGER_DIP)
@@ -54,12 +60,12 @@ def countFingers(hand_landmarks):
     ring = isFingerExtended(hand_landmarks, mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.RING_FINGER_DIP)
     pinky = isFingerExtended(hand_landmarks, mp_hands.HandLandmark.PINKY_TIP, mp_hands.HandLandmark.PINKY_DIP)
 
-    fingers = 0
+    # Counts how many fingers are extended
+    HandData.fingers = 0
     count = [thumb, index, middle, ring, pinky]
     for finger in count:
         if finger:
-            fingers += 1
-    print(count)
+            HandData.fingers += 1
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -75,32 +81,50 @@ while cap.isOpened():
     results = hands.process(frame_rgb)
     
     # Check if hands are detected
-    if results.multi_hand_landmarks:
-        for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
-            # Gets x coordinates of hand every 8 frames
-            hand_x = hand_landmarks.landmark[i].x
-            # Turn coordinates into pixels
+    if results.multi_hand_landmarks and results.multi_handedness:
+        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+            # Hand labels are mirrored so mirror it again
+            if handedness.classification[0].label == "Right":
+                HandData.left_hand = True
+            if handedness.classification[0].label == "Left":
+                HandData.right_hand = True
+
+            hand_x = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x
             hand_x_pixels = int(hand_x * frame_width)
 
             # If there is previous pixel calculate movement else initialize previous pixel
-            if prev_hand_x is not None:
-                movement = hand_x_pixels - prev_hand_x
+            if HandData.prev_hand_x is not None:
+                movement = hand_x_pixels - HandData.prev_hand_x
 
-            prev_hand_x = hand_x_pixels
+            HandData.prev_hand_x = hand_x_pixels
 
-            if frames_elapsed % 8 == 0:
+            # Check only every 8 frames
+            if FRAMES_ELAPSED % 8 == 0:
                 checkWaving(movement)
                 countFingers(hand_landmarks)
 
             # Draw landmarks(lines) on the hand
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    
-    #flip the camera to be like mirror
+    else:
+        HandData.fingers = None
+        HandData.isWaving = None
+        HandData.left_hand = False
+        HandData.right_hand = False
+
+    # Mirror the image camera displays
     frame = cv2.flip(frame, 1)
     # Display the frame with hand landmarks and text
     writeOnImage(frame)
+
+    if HandData.left_hand and not HandData.right_hand:
+        cv2.putText(frame, "Left hand", (10,40), cv2.FONT_HERSHEY_COMPLEX, 0.8,( 0 , 0 , 0 ),2,cv2.LINE_AA)
+        cv2.putText(frame, "Left hand", (10,40), cv2.FONT_HERSHEY_COMPLEX, 0.8,(255,255,255),1,cv2.LINE_AA)
+    if not HandData.left_hand and HandData.right_hand:
+        cv2.putText(frame, "Right hand", (10,40), cv2.FONT_HERSHEY_COMPLEX, 0.8,( 0 , 0 , 0 ),2,cv2.LINE_AA)
+        cv2.putText(frame, "Right hand", (10,40), cv2.FONT_HERSHEY_COMPLEX, 0.8,(255,255,255),1,cv2.LINE_AA)
+
     cv2.imshow('Hand Recognition', frame)
-    frames_elapsed += 1
+    FRAMES_ELAPSED += 1
     
     # Exit when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
